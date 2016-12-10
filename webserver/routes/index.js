6c2973a9c40523e15f5fb37973fa3a90f2cb8e17
Nicholas Buhay <nbuhay@gmail.com>
2016-12-10 11:44:52 -0500

    Feature - Complete skeleton flow for creating new deck.

--- webserver/routes/index.js
+++ webserver/routes/index.js
@@ -4,7 +4,7 @@ var deck = require('../controllers/deck');
 var home = require('../controllers/home');
 
 /* GET home page. */
-router.get('/', function (res) {
+router.get('/', function (req, res) {
   res.render('index');
 });
 
@@ -17,9 +17,24 @@ router.get('/home', function(req, res) {
 	res.render('home', {data: home.getTestUser()});
 });
 
-router.get('/deck', function(req, res) {
-	res.render('deck');
+router.get("/deck/form", function(req, res) {
+	res.render("deckForm");
 });
+
+router.get("/deck/form/create", function(req, res) {
+  res.status(200);
+  res.render("deckCreate");
+});
+
+router.get("/deck/create/cards", function(req, res) {
+	res.status(200);
+	res.render("deckCreateCards");
+});
+
+router.get("/deck/search", function (req, res) {
+  res.status(200);
+  res.send("<h1>Search</h1>")
+})
 
 router.get('/test', function(req, res) {
 	res.status(200);
