99262fb32bb76fb700daea1a1650135d48dad440
Nicholas Buhay <nbuhay@gmail.com>
2016-12-16 20:48:57 -0500

    Improvement - Better mock data injection.  Use mock data JSON objects and global constants.

--- dbAPI/controllers/mockDataCtrl.js
+++ dbAPI/controllers/mockDataCtrl.js
@@ -1,75 +1,56 @@
 var express = require('express');
 var http = require('http');
 var mongoose = require('mongoose');
-var mockUsers = require('../../mockData/users');
+var mockUsers = require('../../mockData/users').users;
+var mockDecks = require('../../mockData/decks').decks;
 var User = require('../models/user');
 var Deck = require('../models/deck');
 var jsonRes = require('../modules/jsonResponse');
+
+var DEFAULT_DECK = 1;
+var DEFAULT_DECK_REQ_OPTS = {
+	port: 3000,
+	path: '/api/deck/name/' + mockDecks[DEFAULT_DECK].name
+}
+var DEFAULT_USER = 0;
 
 // Clear old dummy data:  VERY DESTRUCTIVE
 // Create dummy data:  one user and one deck
 // Associate dummy data:  user has created deck
 // Save data to mongoDb
 module.exports.insert = function (req, res) {
+
 	// Clear old dummy data
 	mongoose.connection.db.dropCollection('decks', function () {
 		console.warn('Decks dropped');
 
 		// Create dummy data
-		var defaultDeck = new Deck({
-			name: 'Harmony',
-			description: 'New learnings from Harmony book.',
-			tags: ['music theory', 'composition'],
-			cards: [
-				{
-					question: 'What tones are in a triad?',
-					answer: 'Tonic, mediant, dominant'
-				},
-				{
-					question: 'Who is Chopin?',
-					answer: 'Solo pianist.'
-				},
-				{
-					question: 'What is your favorite color?',
-					answer: 'Green.'
-				}
-			],
-			favs: 1
-		});
+		var defaultDeck = new Deck(mockDecks[DEFAULT_DECK]);
 
 		// Save deck to mongoDb
 		defaultDeck.save(function (err) {
 			if (err) { 
 				console.error('Error saving defaultDeck') 
 			} else {
+
 				// Clear old dummy data
 				mongoose.connection.db.dropCollection('users', function () {
 					console.warn('Users dropped');
 
-					// get deck doc
-					var options = {
-						port: 3000,
-						path: '/api/deck/name/Harmony'
-					};
-
 					// Associate dummy data
-					http.request(options, function (response) {
+					var callback = function (response) {
 						var str = '';
 						response.on('data', function (chunk) {
 							str += chunk;
 						});
 						response.on('end', function () {
+
 							// Create dummy data
-							var defaultUser = new User({
-								userName: 'Bu',
-								pswd: 'abc123',
-								email: 'nichobu@flash.com',
-								zip: 55555,
-								decks: {
-									// Associate dummy data
-									created: [(JSON.parse(str))._id]
-								}
-							});
+							var defaultUser = new User(mockUsers[DEFAULT_USER]);
+
+							// Associate dummy data
+							defaultUser.decks.created.push((JSON.parse(str))._id);
+
 							// Save user to mongoDb
 							defaultUser.save(function (err) {
 								if (err) { 
@@ -77,11 +58,13 @@ module.exports.insert = function (req, res) {
 									res.status(500);
 								} else {
 									console.log('defaultUser saved')
-									jsonRes.send(res, 200, 'db api');
+									jsonRes.send(res, 200, 'Mock User Creation Successful');
 								}
 							});
 						});
-					}).end();
+					};
+
+					http.request(DEFAULT_DECK_REQ_OPTS, callback).end();
 				});
 			}
 		});
