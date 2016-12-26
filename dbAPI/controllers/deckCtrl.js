145b68da9c660a9b265d38b67fc09ed1f78b0c45
Nicholas Buhay <nbuhay@gmail.com>
2016-12-26 13:42:52 -0500

    Refactor : Deck Controller - Better global usage for response codes.

--- dbAPI/controllers/deckCtrl.js
+++ dbAPI/controllers/deckCtrl.js
@@ -1,4 +1,5 @@
 var CONST = require('../../global');
+const resCode = require('../../global').resCode();
 var mongoose = require('mongoose');
 var Deck = require('../models/deck');
 var jsonRes = require('../modules/jsonResponse');
@@ -7,9 +8,9 @@ module.exports.findAll = (req, res) => {
 	Deck.find((err, decks) => {
 		if (err) {
 			console.error('Error:dbAPI:deckCtrl.findAll:' + err);
-			jsonRes.send(res, CONST.RES('SERVFAIL'), { msg: 'Error:dbAPI:deckCtrl.findAll:' + err });
+			jsonRes.send(res, resCode['SERVFAIL'], { msg: 'Error:dbAPI:deckCtrl.findAll:' + err });
 		}
-		jsonRes.send(res, CONST.RES('OK'), decks);
+		jsonRes.send(res, resCode['OK'], decks);
 	});
 };
 
@@ -17,23 +18,23 @@ module.exports.save = (req, res) => {
 	var deck = new Deck(req.body);
 	deck.save()
 		.then((resolveValue) => {
-			jsonRes.send(res, CONST.RES('OK'), resolveValue);
+			jsonRes.send(res, resCode['OK'], resolveValue);
 		})
 		.then((undefined, rejectValue) => {
-			jsonRes.send(res, CONST.RES('SERVFAIL'), { msg: 'Error:dbAPI:deckCtrl.save:' + rejectValue });
+			jsonRes.send(res, resCode['SERVFAIL'], { msg: 'Error:dbAPI:deckCtrl.save:' + rejectValue });
 		});	
 };
 
-module.exports.findOne = function (req, res) {
+module.exports.findOne = (req, res) => {
 	Deck.findOne(
 		{ name: req.params.name },
 		function (err, deck) {
 			if (err) console.error('Error:dbAPI:deckCtrl.findOne:' + err);
-			jsonRes.send(res, 200, deck);
+			jsonRes.send(res, resCode['OK'], deck);
 		});
 };
 
-module.exports.findById = function (req, res) {
+module.exports.findById = (req, res) => {
 	var promise = new Promise((resolve, reject) => {
 		Deck.findById(req.params._id, (err, deck) => {
 			if (err) reject(err);
@@ -41,9 +42,9 @@ module.exports.findById = function (req, res) {
 		});
 	})
 	.then((resolveValue) => {
-		jsonRes.send(res, 200, resolveValue);
+		jsonRes.send(res, resCode['OK'], resolveValue);
 	})
 	.then(undefined, (rejectValue) => {
-		jsonRes.send(res, 500, 'Error:dbAPI:deckCtrl.findOneId:' + rejectValue);
+		jsonRes.send(res, resCode['SERVFAIL'], { msg: 'Error:dbAPI:deckCtrl.findOneId:' + rejectValue});
 	});
 };
