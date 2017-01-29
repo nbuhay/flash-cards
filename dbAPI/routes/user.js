544820fd8173f047ac29151da246ceb245ab2b24
Nicholas Buhay <nbuhay@gmail.com>
2017-01-29 18:41:21 -0500

    dbAPI : routes/user : TravisCI Fix attempt 3 - Comment out DELETE route to no longer existing function.

--- dbAPI/routes/user.js
+++ dbAPI/routes/user.js
@@ -9,7 +9,7 @@ router.route('/:_id')
 	.head(userCtrl.findById)
 	.get(userCtrl.findById)
 	.put(userCtrl.findByIdAndUpdate)
-	.delete(userCtrl.findOneAndRemove);
+	// .delete(userCtrl.findOneAndRemove);
 
 router.route('/_id/:user_id/learning/deck/_id/:deck_id')
 	.post(userCtrl.saveLearning)
